66d816e477378b0ff2246511fc05dc1fba365937
Jacob Fischer Bak <jacobfbak@gmail.com>
2025-01-13 14:57:40 +0100

    Add responsive canvas dimensions and implement API data loading

--- sketch.js
+++ sketch.js
@@ -1,9 +1,20 @@
+let bredde = 400;   //højden og bredden på skærmen
+let højde = 400;
+let JSON;  //variabel til at gemme data fra API
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(bredde, højde);
 }
 
-
+function preload() {    //henter data fra en API og skriver det på skærmen
+  JSON = loadJSON("https://api.frankfurter.dev/v1/2025-01-12?base=EUR", data => {
+    console.log(data);
+    textAlign(CENTER, CENTER);
+    text(data.base, bredde/2, højde/2);
+  });
+}
 
 function draw() {
   background(220);
+
 }
