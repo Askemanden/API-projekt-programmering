12d6bce56c4a4800d1e7c7e21105543b49b66efd
Jacob Fischer Bak <jacobfbak@gmail.com>
2025-01-13 15:14:07 +0100

    fjernede JSON variabel og text funktion, som ødelagde programmet.

--- sketch.js
+++ sketch.js
@@ -1,6 +1,6 @@
 let bredde = 400;   //højden og bredden på skærmen
 let højde = 400;
-let JSON;  //variabel til at gemme data fra API
+
 
 function setup() {
   createCanvas(bredde, højde);
@@ -26,10 +26,8 @@ function setup() {
 }
 
 function preload() {    //henter data fra en API og skriver det på skærmen
-  JSON = loadJSON("https://api.frankfurter.dev/v1/2025-01-12?base=EUR", data => {
+  loadJSON("https://api.frankfurter.dev/v1/2025-01-12?base=EUR", data => {
     console.log(data);
-    textAlign(CENTER, CENTER);
-    text(data.base, bredde/2, højde/2);
   });
 }
 
