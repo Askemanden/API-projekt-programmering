dca4d7aca0cec25381b25982c66109872bbe99c0
Louis Frichot <louisfrichot2@gmail.com>
2025-01-13 14:57:21 +0100

    Creates a dropdown menu to choose the base valuta.

--- sketch.js
+++ sketch.js
@@ -1,9 +1,22 @@
 function setup() {
   createCanvas(400, 400);
-}
+  let baseValutaDropdown;
+  let valutas = [
+    'AUD','BGN','BRL','CAD','CHF','CNY','CZK','DKK','EUR','GBP','HKD',
+    'HUF','IDR','ILS','INR','ISK','JPY','KRW','MXN','MYR','NOK',
+    'NZD','PHP','PLN','RON','SEK','SGD','THB','TRY','USD','ZAR'
+  ]
 
+  baseValutaDropdown = createSelect(); 
+  baseValutaDropdown.position(0,0); //Remember to change pos coords when ui is made
+  
+//Makes currency options.
+  valutas.forEach(valutas => {
+    baseValutaDropdown.option(valutas);
+  });
 
-
-function draw() {
-  background(220);
+  baseValutaDropdown.changed(() =>  {
+    let selectedValutas = baseValutaDropdown.value();
+    console.log('Selected Valuta:', selectedValutas);
+  });
 }
