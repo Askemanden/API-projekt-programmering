b4da3c81f5c11f084287c1294f2d027fe3ae5af4
Jacob Fischer Bak <jacobfbak@gmail.com>
2025-01-20 13:35:12 +0100

    Refactor currency selection logic

--- sketch.js
+++ sketch.js
@@ -1,6 +1,6 @@
 let bredde = 400;   //højden og bredden på skærmen
 let højde = 400;
-
+let selectedValuta; //Valutaen der er valgt
 
 function setup() {
   createCanvas(bredde, højde);
@@ -16,18 +16,22 @@ function setup() {
   
 //Makes currency options.
   valutas.forEach(valutas => {
-    baseValutaDropdown.option(valutas);
+    if (valutas) {
+      if (valutas !== selectedValuta) {
+        baseValutaDropdown.option(valutas);
+      }
+    }
   });
 
   baseValutaDropdown.changed(() =>  {
-    let selectedValutas = baseValutaDropdown.value();
-    console.log('Selected Valuta:', selectedValutas);
+    selectedValuta = baseValutaDropdown.value();
+    console.log('Selected Valuta:', selectedValuta);
   });
 }
 
 function preload() {    //henter data fra en API og skriver det på skærmen
   loadJSON("https://api.frankfurter.dev/v1/2025-01-12?base=EUR", data => {
-    console.log(data);
+    
   });
 }
 
